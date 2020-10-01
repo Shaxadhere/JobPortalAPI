b9b43f49dabefeef2ab86c2275739d14e3650bbc
Shehzad <shaxad.here@gmail.com>
2020-10-01 17:37:31 +0500

    add skills

--- controller/worker/addSkills.php
+++ controller/worker/addSkills.php
@@ -2,9 +2,38 @@
 
 include_once('../../config.php');
 
-$skills = $_REQUEST['skills']
+$conn = connect();
+
+$errors = array();
+
+$skills = $_REQUEST['skills'];
+$workerID = $_REQUEST['workerID'];
+
+if(empty($skills)){
+    array_push($errors, "Select at least on skill");
+}
+
+if(empty($workerID)){
+    array_push($errors, "Your session expired, please login again");
+}
+
+if($errors == null){
+    foreach ($skills as $skill) {
+        insertData(
+            "tbl_worker_skils",
+            array(
+                "SkillID",
+                "WorkerID"
+            ),
+            array(
+                $skill,
+                $workerID
+            ),
+            $conn
+        );
+    }
+}
 
 
 ?>
 
-cook, Driver, Chowkidar, Daily wage labor, mason, plumber
---
+++ db/sampleskills.sql
@@ -0,0 +1,5 @@
+INSERT INTO `tbl_skills`(`SkillName`) VALUES ('Cook');
+INSERT INTO `tbl_skills`(`SkillName`) VALUES ('Chowkidar');
+INSERT INTO `tbl_skills`(`SkillName`) VALUES ('Daily Wage Labor');
+INSERT INTO `tbl_skills`(`SkillName`) VALUES ('Mason');
+INSERT INTO `tbl_skills`(`SkillName`) VALUES ('Plumber');
