493c4ad6806141cd880868d4148c7b1c8c664da1
Shehzad <shaxad.here@gmail.com>
2020-10-04 18:08:35 +0500

    db config

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ include_once('assets/rapid.php');
 
 function connect(){
     define("server", "localhost");
-    define("usr","root");
-    define("pas","");
+    define("usr","oreo");
+    define("pas","786786PkPk");
     define("data","db_workfinder");
     $connection = mysqli_connect(server, usr, pas, data) or die("failed to connect to database");
     return ($connection);
