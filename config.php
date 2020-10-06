81fedab8da576c26db11f387f3a33ac979f71ee4
Shehzad <shaxad.here@gmail.com>
2020-10-06 22:08:26 +0500

    construct()

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 
 include_once('assets/rapid.php');
+include_once('functions/main.php');
 
 
+//default database connection method
 function connect(){
     define("server", "localhost");
     define("usr","root");
@@ -11,5 +13,8 @@ function connect(){
     $connection = mysqli_connect(server, usr, pas, data) or die("failed to connect to database");
     return ($connection);
 }
+
+//constructs application
+construct(connect());
 
 ?>
