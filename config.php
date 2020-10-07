e93e062bd7203c19e79f3046a1dc3a59f8427d32
Shehzad <shaxad.here@gmail.com>
2020-10-07 13:32:43 +0500

    config

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ function connect(){
 }
 
 // $query = "INSERT INTO tbl_admin (FullName, Email, Password, Picture)
-// SELECT * FROM (SELECT 'admin', 'admin@workfinder.com', '123', 'http://shaxad.com/apis/assets/avatar.jpg') AS tmp
+// SELECT * FROM (SELECT 'admin', 'admin@workfinder.com', '1234', 'http://shaxad.com/apis/assets/avatar.jpg') AS tmp
 // WHERE NOT EXISTS (
 // SELECT * FROM tbl_admin
 // ) LIMIT 1";
