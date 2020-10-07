8802fc5461ddc314fe005f13a73686b20edd5761
Shehzad <shaxad.here@gmail.com>
2020-10-07 13:15:12 +0500

    connection

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ include_once('functions/main.php');
 function connect(){
     define("server", "localhost");
     define("usr","oreo");
-    define("pas","786786pkpk");
+    define("pas","786786PkPk");
     define("data","db_workfinder");
     $connection = mysqli_connect(server, usr, pas, data) or die("failed to connect to database");
     return ($connection);
