b36864bb9f6b4a6ba8e6d4fc8b879ad8fa841c96
Shehzad <shaxad.here@gmail.com>
2020-10-08 17:46:38 +0500

    upload path

--- controller/worker/uploadCnic.php
+++ controller/worker/uploadCnic.php
@@ -56,7 +56,7 @@ if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "../img/imageDirectory/" . $newfilename)) {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "../../uploads/worker/" . $newfilename)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
